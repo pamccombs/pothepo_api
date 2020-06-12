2ce16ab1a2aeac5f1b0e9f0f7633ca915616bf9d
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-06-12 11:42:01 -0400

    update video model spec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -14,6 +14,6 @@ RSpec.describe Video, type: :model do
   it { should validate_presence_of(:city) }
   it { should validate_presence_of(:state) }
   it { should validate_presence_of(:zip_code) }
-  it { should validate_presence_of(:date) }
-  it { should validate_presence_of(:time) }
+  # it { should validate_presence_of(:date) }
+  # it { should validate_presence_of(:time) }
 end
