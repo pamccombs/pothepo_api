ee095d320124e91de511962ab511a2c24f9d17cd
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-06-12 10:57:36 -0400

    add logged in and logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,5 +24,15 @@ class SessionsController < ApplicationController
         logged_in: true,
         user: @current_user
       }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json { status: 200, logged_out: true }
   end
 end
